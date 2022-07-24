87ff128b36282806ef1638bdb007b38241f5f93f
Emmanuel  Atakos <101354178+Atakos254@users.noreply.github.com>
2022-07-24 07:30:50 +0300

    Create 1-square.py

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+# 1-square.py
+"""Define a class Square."""
+
+
+class Square:
+    """Represent a square."""
+
+    def __init__(self, size):
+        """Initialize a new Square.
+        Args:
+            size (int): The size of the new square.
+        """
+        self.__size = size
+Footer
+© 2022 GitHub, Inc.
+Footer navigation
+Terms
